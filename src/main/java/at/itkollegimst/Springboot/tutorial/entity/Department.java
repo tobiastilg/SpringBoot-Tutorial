1579381dc87fd385335fff0c96d713f029cb5d1b
tobiastilg <tobi.tilg@gmail.com>
2021-10-26 17:52:14 +0200

    added Lombok framework

--- src/main/java/at/itkollegimst/Springboot/tutorial/entity/Department.java
+++ src/main/java/at/itkollegimst/Springboot/tutorial/entity/Department.java
@@ -1,5 +1,9 @@
 package at.itkollegimst.Springboot.tutorial.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +13,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity //Class can interact with Database using JPA
+@Data //Lombok (es gibt auch @-Notations für zB Getter, falls man nur diese benötigen würde)
+@NoArgsConstructor //DefaultConstructor
+@AllArgsConstructor //Konstruktor mit allen Datenfeldern
+@Builder //entire Builder Pattern (laut Video momentan nicht wichtig zu verstehen)
 public class Department {
 
     @Id
@@ -21,56 +29,4 @@ public class Department {
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
-
-    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    public Department() {
-    }
-
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentId=" + departmentId +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
 }
